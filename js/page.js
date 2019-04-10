0cdf89c23bd53c325cb61140ee4d7f615a183891
Ryan Perrillo <rynomat14@gmail.com>
2019-04-10 11:48:30 -0400

    img changes

--- js/page.js
+++ js/page.js
@@ -33,3 +33,14 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   indicator[slideIndex-1].className += " active";
 }
+//* New Modal Scripts
+$(".button").on("click", function() {
+  var modal = $(this).data("modal");
+  $(modal).show();
+});
+$(".modal").on("click", function(e) {
+  var className = e.target.className;
+  if(className === "modal" || className === "close"){
+    $(this).closest(".modal").hide();
+  }
+});
